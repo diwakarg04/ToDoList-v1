1347fcc459bb287acdde7acde01e6fb296b26c6b
diwakarg04 <117665331+diwakarg04@users.noreply.github.com>
2023-04-14 10:18:06 +0530

    second Commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express") ;
+
+const bodyParser  = require("body-parser") ;
+
+const app = express() ;
+
+app.use(bodyParser.urlencoded({extended: true})) ;
+
+app.use(express.static("public")) ;
+
+app.set("view engine" , "ejs") ;
+
+app.listen(2500 , function(){
+    console.log("Server running at port 2500") ;
+});
+
+let items = ["Buy Food" , "Cook Food" , "Eat Food"] ;
+
+app.get("/" , function(req,res){
+    
+    let today = new Date() ;
+
+    // creating an Object
+    let options ={
+        weekday: "long" ,
+        day: "numeric" ,
+        month: "long"
+    } ;
+
+    let day = today.toLocaleDateString("en-US" , options ) ;
+   
+    res.render("list" , {kindOfDay : day , newlistitems: items }) ;
+})
+
+app.post("/" , function(req, res){
+
+    let item = req.body.newItem ;
+
+    items.push(item) ;
+
+    res.redirect("/");
+})
